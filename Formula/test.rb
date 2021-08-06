54d12751c81aba7dd53f0ef21bc1a9ebe0ea1f36
Miloslav Nenadal <nenadalm@gmail.com>
2021-08-06 21:40:55 +0200

    Make sure the linux binary gets installed only on linux

--- Formula/test.rb
+++ Formula/test.rb
@@ -1,8 +1,13 @@
 class Test < Formula
   desc "Testing formula"
   homepage "https://github.com/nenadalm/test"
-  url "https://github.com/nenadalm/test/releases/download/v2021.06.23.2010/test-linux-amd64"
-  sha256 "0e186e8e2e12d605f2d8a4638b57298b53163336fcb576dfffa2e4a28656ae3f"
+
+  if OS.linux?
+    url "https://github.com/nenadalm/test/releases/download/v2021.06.23.2010/test-linux-amd64"
+    sha256 "0e186e8e2e12d605f2d8a4638b57298b53163336fcb576dfffa2e4a28656ae3f"
+  else
+    raise "This OS is not supported."
+  end
 
   bottle :unneeded
 
