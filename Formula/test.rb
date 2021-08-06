82e23b40d208d69d3a7592d39262a4e8c7b0a849
Miloslav Nenadal <nenadalm@gmail.com>
2021-08-06 21:25:50 +0200

    Initial commit

---
+++ Formula/test.rb
@@ -0,0 +1,12 @@
+class Test < Formula
+  desc "Testing formula"
+  homepage "https://github.com/nenadalm/test"
+  url "https://github.com/nenadalm/test/releases/download/v2021.06.23.2010/test-linux-amd64"
+  sha256 "0e186e8e2e12d605f2d8a4638b57298b53163336fcb576dfffa2e4a28656ae3f"
+
+  bottle :unneeded
+
+  def install
+    bin.install "test-linux-amd64" => "nenadalm-test"
+  end
+end
