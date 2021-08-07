409695fdbed85b26662c29c914279c603456756d
github-action <github-action@example.com>
2021-08-07 17:38:54 +0000

    Update test formula

--- Formula/test.rb
+++ Formula/test.rb
@@ -1,11 +1,11 @@
 class Test < Formula
   desc "Testing formula"
   homepage "https://github.com/nenadalm/test"
-  version "2021.08.07.1729"
+  version "2021.08.07.1737"
 
   if OS.linux?
-    url "https://github.com/nenadalm/test/releases/download/v/test-linux-amd64"
-    sha256 "044a87e70d0d0e5cfbd35ae4f86a036e26957ea8ec0e7b16b2735907beedf742"
+    url "https://github.com/nenadalm/test/releases/download/v2021.08.07.1737/test-linux-amd64"
+    sha256 "f3c4d9fbf6fd06ef45d28fae57af85d9d9662843feed55b9cbdf17db0c75d054"
   else
     raise "This OS is not supported."
   end
